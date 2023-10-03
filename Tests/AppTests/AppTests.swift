ad0bce350c175fe909d3f9f69ab986b14a10f382
Daniel Romero <daniel.romero0328@gmail.com>
2023-10-03 14:49:26 -0400

    demonstration test failure with all permutations of API usage

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -13,7 +13,47 @@ import XCTest
 @MainActor
 class AppTests: XCTestCase {
 
-  func test_onAppear_createsInitialModels() async {
+  func test_onAppear_createsInitialModels_1() async {
+    let expectedModels: [PersistenceModel] = [.init(foo: "bar")]
+    
+    let store = TestStore(
+      initialState: .init(),
+      reducer: { AppReducer() },
+      withDependencies: {
+        $0.someDependency.fetch = { return [] }
+        $0.someDependency.create = { _ in }
+      }
+    )
+    
+    await store.send(.onAppear)
+    store.dependencies.someDependency.fetch = { return expectedModels }
+    await store.receive(.initialFetch(.success([])))
+    await store.receive(.createInitialModels)
+    await store.receive(.modelsLoaded(.success(expectedModels)))
+  }
+  
+  func test_onAppear_createsInitialModels_2() async {
+    let expectedModels: [PersistenceModel] = [.init(foo: "bar")]
+    
+    let store = TestStore(
+      initialState: .init(),
+      reducer: { AppReducer() },
+      withDependencies: {
+        $0.someDependency.fetch = { return [] }
+        $0.someDependency.create = { _ in }
+      }
+    )
+    
+    await store.send(.onAppear)
+    await store.receive(.initialFetch(.success([])))
+    store.dependencies.someDependency.fetch = { return expectedModels }
+    await store.receive(.createInitialModels)
+    await store.receive(.modelsLoaded(.success(expectedModels)))
+  }
+  
+  func test_onAppear_createsInitialModels_3() async {
+    let expectedModels: [PersistenceModel] = [.init(foo: "bar")]
+    
     let store = TestStore(
       initialState: .init(),
       reducer: { AppReducer() },
@@ -26,6 +66,81 @@ class AppTests: XCTestCase {
     await store.send(.onAppear)
     await store.receive(.initialFetch(.success([])))
     await store.receive(.createInitialModels)
-    await store.receive(.modelsLoaded(.success([])))
+    store.dependencies.someDependency.fetch = { return expectedModels }
+    await store.receive(.modelsLoaded(.success(expectedModels)))
+  }
+  
+  func test_onAppear_createsInitialModels_4() async {
+    let expectedModels: [PersistenceModel] = [.init(foo: "bar")]
+    
+    let store = TestStore(
+      initialState: .init(),
+      reducer: { AppReducer() },
+      withDependencies: {
+        $0.someDependency.fetch = { return [] }
+        $0.someDependency.create = { _ in }
+      }
+    )
+    
+
+    await store.send(.onAppear)
+    
+    await store.withDependencies {
+      $0.someDependency.fetch = { return expectedModels }
+    } operation: {
+      await store.receive(.initialFetch(.success([])))
+    }
+    
+    await store.receive(.createInitialModels)
+    await store.receive(.modelsLoaded(.success(expectedModels)))
+  }
+  
+  func test_onAppear_createsInitialModels_5() async {
+    let expectedModels: [PersistenceModel] = [.init(foo: "bar")]
+    
+    let store = TestStore(
+      initialState: .init(),
+      reducer: { AppReducer() },
+      withDependencies: {
+        $0.someDependency.fetch = { return [] }
+        $0.someDependency.create = { _ in }
+      }
+    )
+    
+
+    await store.send(.onAppear)
+    await store.receive(.initialFetch(.success([])))
+    
+    await store.withDependencies {
+      $0.someDependency.fetch = { return expectedModels }
+    } operation: {
+      await store.receive(.createInitialModels)
+    }
+    
+    await store.receive(.modelsLoaded(.success(expectedModels)))
+  }
+  
+  func test_onAppear_createsInitialModels_6() async {
+    let expectedModels: [PersistenceModel] = [.init(foo: "bar")]
+    
+    let store = TestStore(
+      initialState: .init(),
+      reducer: { AppReducer() },
+      withDependencies: {
+        $0.someDependency.fetch = { return [] }
+        $0.someDependency.create = { _ in }
+      }
+    )
+    
+
+    await store.send(.onAppear)
+    await store.receive(.initialFetch(.success([])))
+    await store.receive(.createInitialModels)
+    
+    await store.withDependencies {
+      $0.someDependency.fetch = { return expectedModels }
+    } operation: {
+      await store.receive(.modelsLoaded(.success(expectedModels)))
+    }
   }
 }
